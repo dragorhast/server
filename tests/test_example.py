ecb67e49b50f1787f536ec2581cd7c7133bfab9d
Alexander Lyon <arlyon@me.com>
2018-10-25 18:13:54 +0100

    Mark tests as asyncio

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
 
+import pytest
+
 from server.pricing import get_price
 
 
+@pytest.mark.asyncio
 async def test_pricing():
-    assert await get_price("eh47bl", datetime.now() - timedelta(hours=6), "le33aw", datetime.now()) == 0.6
+    assert await get_price("eh47bl", datetime.now() - timedelta(hours=6), "le33aw", datetime.now()) == 4.55
