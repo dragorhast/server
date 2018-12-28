29b5858d110f91e8b2312302a647522a314735aa
Alexander Lyon <arlyon@me.com>
2018-12-28 00:19:30 +0000

    Add me forwarding

--- server/views/users.py
+++ server/views/users.py
@@ -77,6 +77,61 @@ class UserIssuesView(BaseView):
 
 class MeView(BaseView):
     """
-    Gets the data for the currently authenticated user/
+    Gets the data for the currently authenticated user.
     """
-    url = "/users/me"
+
+    url = "/users/me{tail:.*}"
+    name = "me"
+
+    @staticmethod
+    async def me_handler(request: Request):
+        """
+        Accepts all types of request, does some checking against the user, and forwards them on to the appropriate user.
+        """
+
+        if "Authorization" not in request.headers or not request.headers["Authorization"].startswith("Bearer "):
+            response_schema = JSendSchema()
+            return web.json_response(response_schema.dump({
+                "status": JSendStatus.FAIL,
+                "data": {"json": "You must supply your firebase token."}
+            }), status=401)
+        else:
+            token = request.headers["Authorization"][7:]
+            # todo process firebase id
+
+        user = await get_user(token)
+
+        if user is None:
+            response_schema = JSendSchema()
+            create_user_url = str(request.app.router['users'].url_for())
+            return web.json_response(response_schema.dump({
+                "status": JSendStatus.FAIL,
+                "data": {
+                    "authorization": "User does not exist. Please use your token to create a user and try again.",
+                    "url": create_user_url,
+                    "method": "POST"
+                }
+            }), status=401)
+
+        concrete_url = MeView._get_concrete_user_url(request.path, request.match_info.get("tail"), user)
+        raise web.HTTPFound(concrete_url)
+
+    @staticmethod
+    def _get_concrete_user_url(path, tail, user) -> str:
+        """
+        Given a relative "me" url, and a user, rewrites the url to a concrete user.
+
+        :param path: The current path of the "me" url.
+        :param tail: The tail section of the url (after the "me")
+        :param user: The user to rewrite to.
+        """
+        url_without_tail = path[:-len(tail)]
+        id = user.id
+        user_url = url_without_tail[:-2] + str(id)
+        return user_url + tail
+
+    @classmethod
+    def register(cls, router: UrlDispatcher, base: Optional[str] = None):
+        """Overrides the register function to register the me_handler for all request types."""
+        url = base + cls.url if base is not None else cls.url
+        router.add_route("*", url, cls.me_handler, name=cls.name)
