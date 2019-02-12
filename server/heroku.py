4ef3175760626f4bc68789a39bc3680580331139
Alexander Lyon <arlyon@me.com>
2019-02-12 16:13:12 +0000

    Replace postgres database with postgis in DATABASE_URL on heroku

--- server/heroku.py
+++ server/heroku.py
@@ -2,6 +2,6 @@ import os
 
 from server.app import build_app
 
-database_url = os.getenv("DATABASE_URL", None)
+database_url = os.getenv("DATABASE_URL", None).replace("postgres", "postgis", 1)
 
 app = build_app(database_url)
